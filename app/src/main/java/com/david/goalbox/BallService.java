fa3b174a7d467bd36abae734af2345f2ce82fb25
David mochoge <kingkgdave@gmail.com>
2019-07-11 21:44:11 +0300

    add changes to gradle properties and constants

--- app/src/main/java/com/david/goalbox/BallService.java
+++ app/src/main/java/com/david/goalbox/BallService.java
@@ -16,46 +16,45 @@ import okhttp3.Response;
 
 public class BallService {
 
-    public static void findSeason(String Season, Callback callback) {
-
-        OkHttpClient client = new OkHttpClient.Builder()
-                .build();
-
-        HttpUrl.Builder urlBuilder = HttpUrl.parse(Constants.BALL_BASE_URL).newBuilder();
-        urlBuilder.addQueryParameter(Constants.BALL_SEASON_QUERY_PARAMETER, Season);
+    //get All leagues
+    public static void getAllLeagues(Callback callback) {
+        OkHttpClient client = new OkHttpClient.Builder().build();
+        HttpUrl.Builder urlBuilder = HttpUrl.parse("https://api-football-v1.p.rapidapi.com/v2/leagues/season/2019").newBuilder();
         String url = urlBuilder.build().toString();
-
         Request request = new Request.Builder()
                 .url(url)
-                .header("X-RapidAPI-Key", Constants.BallToken)
+                .addHeader("X-RapidAPI-Host", "api-football-v1.p.rapidapi.com")
+                .addHeader("X-RapidAPI-Key", Constants.X_RapidAPI_Key)
                 .build();
-
         Call call = client.newCall(request);
         call.enqueue(callback);
     }
 
-    public ArrayList<Leagues> processResults(Response response){
-        ArrayList<Leagues> ligi = new ArrayList<>();
+    public ArrayList<Leagues> processGetAllLeaguesResults(Response response) {
+        ArrayList<Leagues> allLeagues = new ArrayList<>();
         try {
             String jsonData = response.body().string();
-            JSONObject leaguesJSON = new JSONObject(jsonData);
-            JSONArray leaguesJSONa = leaguesJSON.getJSONArray("api");
-            if (response.isSuccessful()){
-                for (int i = 0; i < leaguesJSONa.length(); i++){
-                    JSONObject leagJSON = leaguesJSONa.getJSONObject(i);
-                    String name = leagJSON.getString("name");
-                    String country = leagJSON.getString("country");
-                    String season = leagJSON.getString("season");
-                    String start = leagJSON.getString("season_start");
-                    String end = leagJSON.getString("season_end");
-                    String url = leagJSON.getString("logo");
+            JSONObject leaguesJson = new JSONObject(jsonData).getJSONObject("api");
+            JSONArray leaguesJsonArray = leaguesJson.getJSONArray("leagues");
+            if (response.isSuccessful()) {
+                for (int i = 0; i < leaguesJsonArray.length(); i++) {
+                    JSONObject leagueJSON = leaguesJsonArray.getJSONObject(i);
 
+                    int leagueId = leagueJSON.getInt("league_id");
+                    String name = leagueJSON.getString("name");
 
+                    String country = leagueJSON.getString("country_code");
+                    String countryCode = leagueJSON.getString("country_code");
+                    int season = leagueJSON.getInt("season");
+                    String seasonStart = leagueJSON.getString("season_start");
+                    String seasonEnd = leagueJSON.getString("season_end");
+                    String logo = leagueJSON.getString("logo");
+                    String flag = leagueJSON.getString("flag");
+                    int standings = leagueJSON.getInt("standings");
+                    int isCurrent = leagueJSON.getInt("is_current");
 
-
-                    Leagues lig = new Leagues(name,country,season, start, end,url
-                    );
-                    ligi.add(lig);
+                    Leagues league = new Leagues(leagueId,name, country,countryCode,season,seasonStart,seasonEnd,logo,flag,standings, isCurrent);
+                    allLeagues.add(league);
                 }
             }
         } catch (IOException e) {
@@ -63,7 +62,77 @@ public class BallService {
         } catch (JSONException e) {
             e.printStackTrace();
         }
-        return ligi;
+        return allLeagues;
+    }
+
+
+    //get Fixtures
+    public static void getFixtures(Callback callback,String leagueId) {
+        OkHttpClient client = new OkHttpClient.Builder().build();
+
+        HttpUrl.Builder urlBuilder = HttpUrl.parse("https://api-football-v1.p.rapidapi.com/v2/fixtures/league/"+leagueId).newBuilder();
+        String url = urlBuilder.build().toString();
+        Request request = new Request.Builder()
+                .url(url)
+                .addHeader("X-RapidAPI-Host", "api-football-v1.p.rapidapi.com")
+                .addHeader("X-RapidAPI-Key", Constants.X_RapidAPI_Key)
+                .build();
+        Call call = client.newCall(request);
+        call.enqueue(callback);
+    }
+    public ArrayList<Fixtures> processGetFixturesResults(Response response) {
+        ArrayList<Fixtures> allFixture = new ArrayList<>();
+
+        try {
+            String jsonData = response.body().string();
+            JSONObject fixturesJson = new JSONObject(jsonData).getJSONObject("api");;
+            JSONArray fixturesJsonArray = fixturesJson.getJSONArray("fixtures");
+            if (response.isSuccessful()) {
+                for (int i = 0; i < fixturesJsonArray.length(); i++) {
+                    JSONObject fixtureJSON = fixturesJsonArray.getJSONObject(i);
+
+                    int fixtureId = fixtureJSON.getInt("fixture_id");
+                    int leagueId = fixtureJSON.getInt("league_id");
+                    String event_date = fixtureJSON.getString("event_date");
+                    long eventTimestamp = fixtureJSON.getLong("event_timestamp");
+                    long firstHalfStart = fixtureJSON.getLong("firstHalfStart");
+                    long secondHalfStart = fixtureJSON.getLong("secondHalfStart");
+                    String round = fixtureJSON.getString("round");
+                    String status = fixtureJSON.getString("status");
+                    String venue = fixtureJSON.getString("venue");
+                    String referee = fixtureJSON.getString("referee");
+
+                    String homeTeamId = fixtureJSON.getJSONObject("homeTeam").getString("team_id");
+                    String homeTeamName= fixtureJSON.getJSONObject("homeTeam").getString("team_name");
+                    String homeTeamLogo = fixtureJSON.getJSONObject("homeTeam").getString("logo");
+
+                    String awayTeamId = fixtureJSON.getJSONObject("awayTeam").getString("team_id");
+                    String awayTeamName = fixtureJSON.getJSONObject("awayTeam").getString("team_name");
+                    String awayTeamLogo = fixtureJSON.getJSONObject("awayTeam").getString("logo");
+
+                    int goalsHomeTeam = fixtureJSON.getInt("goalsHomeTeam");
+                    int goalsAwayTeam = fixtureJSON.getInt("goalsAwayTeam");
+
+                    String halfTime = fixtureJSON.getJSONObject("score").getString("halftime");
+                    String fullTime = fixtureJSON.getJSONObject("score").getString("fulltime");
+                    String extraTime = fixtureJSON.getJSONObject("score").getString("extratime");
+                    String penalty = fixtureJSON.getJSONObject("score").getString("penalty");
+
+                    Fixtures fixture = new Fixtures(fixtureId,leagueId,event_date,eventTimestamp,firstHalfStart,secondHalfStart,round,status,venue,referee,homeTeamId,homeTeamName,homeTeamLogo,awayTeamId,awayTeamName,awayTeamLogo,goalsHomeTeam,goalsAwayTeam,halfTime,fullTime,extraTime,penalty);
+                    allFixture.add(fixture);
+                }
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+        return allFixture;
+
     }
 
 
--- app/src/main/java/com/david/goalbox/Constants.java
+++ app/src/main/java/com/david/goalbox/Constants.java
@@ -1,7 +1,6 @@
 package com.david.goalbox;
 
 public class Constants {
-    public static final String BallToken = BuildConfig.BallToken;
-    public static final String BALL_BASE_URL = "https://api-football-v1.p.rapidapi.com/v2/leagues/season/2019";
-    public static final String BALL_SEASON_QUERY_PARAMETER = "Season";
+    public static final String X_RapidAPI_Key = BuildConfig.X_RapidAPI_Key;
+
 }
